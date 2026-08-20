8e33fd83678f370298c6c78a9d4f29001f4bdfd3
involvex <39644169+involvex@users.noreply.github.com>
2026-08-21 00:05:19 +0200

    ci/local: bump Kotlin plugin to 2.2.20 to meet Flutter minimum

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -21,7 +21,7 @@ plugins {
     // Use AGP 8.x compatible with Gradle 8.6 in CI for now; we'll migrate to AGP 9 in a dedicated branch.
     id("com.android.application") version "8.11.1" apply false
     // Use Kotlin 1.9.20 to match embedded-kotlin expectations on this toolchain.
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
 include(":app")
